b60f9e34ba925eb8a3962a115e0739a83294cf1c
Dmitri Mogilevski <dmogilevski@gmail.com>
2020-01-17 11:07:08 -0600

    Create and BatchUpdate implemented

--- GoogleSheets/Program.cs
+++ GoogleSheets/Program.cs
@@ -16,10 +16,6 @@ namespace GoogleSheets
 {
 	internal class Program
 	{
-		// If modifying these scopes, delete your previously saved credentials
-		// at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
-		static string[] Scopes = {SheetsService.Scope.SpreadsheetsReadonly};
-		static string ApplicationName = "Google Sheets API .NET Quickstart";
 
 		private static async Task Main(string[] args)
 		{
@@ -28,7 +24,7 @@ namespace GoogleSheets
 
 		private static async Task CreateSheetAsync()
 		{
-			ServiceAccountCredential credential = createGoogleCredential();
+			ServiceAccountCredential credential = CreateGoogleCredential();
 
 			var sheetsService = new SheetsService(new BaseClientService.Initializer
 			{
@@ -36,6 +32,15 @@ namespace GoogleSheets
 				HttpClientInitializer = credential
 			});
 
+			Spreadsheet createResponse = await CreateAsync(sheetsService);
+
+			BatchUpdateValuesResponse batchUpdateValuesResponse = await BatchUpdateAsync(sheetsService, createResponse.SpreadsheetId);
+
+			Console.ReadKey();
+		}
+
+		private static async Task<Spreadsheet> CreateAsync(SheetsService sheetsService)
+		{
 			var requestBody = new Spreadsheet
 			{
 				Properties = new SpreadsheetProperties
@@ -44,23 +49,79 @@ namespace GoogleSheets
 				}
 			};
 
-			SpreadsheetsResource.CreateRequest request = sheetsService.Spreadsheets.Create(requestBody);
+			SpreadsheetsResource.CreateRequest createRequest = sheetsService.Spreadsheets.Create(requestBody);
 
-			Spreadsheet response = await request.ExecuteAsync();
+			Spreadsheet createResponse = await createRequest.ExecuteAsync();
+			Console.WriteLine("Create sheet response");
 
-			Console.WriteLine(JsonConvert.SerializeObject(response));
-
-			Console.ReadKey();
+			Console.WriteLine(JsonConvert.SerializeObject(createResponse));
+			return createResponse;
 		}
 
-		private static ServiceAccountCredential createGoogleCredential()
+		private static async Task<BatchUpdateValuesResponse> BatchUpdateAsync(SheetsService sheetsService, string spreadsheetId)
+		{
+			// How the input data should be interpreted.
+			const string valueInputOption = "USER_ENTERED"; // TODO: Update placeholder value.
+
+			// The new values to apply to the spreadsheet.
+			var data = new List<ValueRange>
+			{
+				new ValueRange
+				{
+					Range = "Sheet1!A1:B1",
+					Values = new List<IList<object>>
+					{
+						new List<object>
+						{
+							"First name",
+							"Last name"
+						}
+					}
+				},
+				new ValueRange
+				{
+					Range = "Sheet1!A2:B2",
+					Values = new List<IList<object>>
+					{
+						new List<object>
+						{
+							"Jack",
+							"Porter"
+						}
+					}
+				}
+			};
+
+			// TODO: Assign values to desired properties of `requestBody`:
+			var requestBody = new BatchUpdateValuesRequest
+			{
+				ValueInputOption = valueInputOption,
+				Data = data,
+				IncludeValuesInResponse = true
+			};
+
+			SpreadsheetsResource.ValuesResource.BatchUpdateRequest batchUpdateRequest = sheetsService.Spreadsheets.Values.BatchUpdate(requestBody, spreadsheetId);
+
+			BatchUpdateValuesResponse batchUpdateResponse = await batchUpdateRequest.ExecuteAsync();
+			Console.WriteLine("Batch update sheet response");
+			Console.WriteLine(JsonConvert.SerializeObject(batchUpdateResponse));
+
+			return batchUpdateResponse;
+		}
+
+		private static ServiceAccountCredential CreateGoogleCredential()
 		{
 			// GoogleCredential.FromFile("tempo-265222-88dada7b17c3.json");
 			ServiceAccountCredential credential;
-			string[] Scopes = {SheetsService.Scope.Spreadsheets};
-			string serviceAccountEmail = "tempo-api@tempo-265222.iam.gserviceaccount.com";
-			string jsonfile = "tempo-265222-88dada7b17c3.json";
-			using (Stream stream = new FileStream(@jsonfile, FileMode.Open, FileAccess.Read, FileShare.Read))
+			string[] scopes =
+			{
+				SheetsService.Scope.Spreadsheets,
+				SheetsService.Scope.Drive,
+				SheetsService.Scope.DriveFile
+			};
+			var serviceAccountEmail = "tempo-api@tempo-265222.iam.gserviceaccount.com";
+			var jsonFile = "tempo-265222-88dada7b17c3.json";
+			using (Stream stream = new FileStream(@jsonFile, FileMode.Open, FileAccess.Read, FileShare.Read))
 			{
 				credential = (ServiceAccountCredential)
 					GoogleCredential.FromStream(stream).UnderlyingCredential;
@@ -69,7 +130,7 @@ namespace GoogleSheets
 				{
 					User = serviceAccountEmail,
 					Key = credential.Key,
-					Scopes = Scopes
+					Scopes = scopes
 				};
 				credential = new ServiceAccountCredential(initializer);
 			}
