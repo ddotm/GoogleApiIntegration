b3692361159e54924f9887003957fb2e709ba672
Dmitri Mogilevski <dmogilevski@gmail.com>
2020-01-17 13:47:04 -0600

    Google Drive API -- List Files

--- GoogleSheets/Program.cs
+++ GoogleSheets/Program.cs
@@ -2,6 +2,8 @@
 using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
 using Google.Apis.Util.Store;
 using Newtonsoft.Json;
 using System;
@@ -16,7 +18,6 @@ namespace GoogleSheets
 {
 	internal class Program
 	{
-
 		private static async Task Main(string[] args)
 		{
 			await CreateSheetAsync();
@@ -36,7 +37,46 @@ namespace GoogleSheets
 
 			BatchUpdateValuesResponse batchUpdateValuesResponse = await BatchUpdateAsync(sheetsService, createResponse.SpreadsheetId);
 
+			await ListFilesAsync(credential, createResponse.SpreadsheetId, "dmitri.mogilevski@paradigmagency.com");
+
 			Console.ReadKey();
+		}
+
+		/// <summary>
+		/// Google Drive API -- List Files
+		/// </summary>
+		/// <param name="credential"></param>
+		/// <param name="fileId"></param>
+		/// <param name="email"></param>
+		/// <returns></returns>
+		private static async Task ListFilesAsync(ServiceAccountCredential credential, string fileId, string email)
+		{
+			var service = new DriveService(new BaseClientService.Initializer
+			{
+				HttpClientInitializer = credential,
+				ApplicationName = "TempoApi"
+			});
+
+			FilesResource.ListRequest listRequest = service.Files.List();
+			listRequest.PageSize = 10;
+			listRequest.Fields = "nextPageToken, files(id, name)";
+
+			// List files.
+			IList<Google.Apis.Drive.v3.Data.File> files = (await listRequest.ExecuteAsync())
+				.Files;
+			Console.WriteLine("Files:");
+			if (files != null && files.Count > 0)
+			{
+				foreach (var file in files)
+				{
+					Console.WriteLine("{0} ({1})", file.Name, file.Id);
+				}
+			}
+			else
+			{
+				Console.WriteLine("No files found.");
+			}
+
 		}
 
 		private static async Task<Spreadsheet> CreateAsync(SheetsService sheetsService)
@@ -57,6 +97,7 @@ namespace GoogleSheets
 			Console.WriteLine(JsonConvert.SerializeObject(createResponse));
 			return createResponse;
 		}
+
 
 		private static async Task<BatchUpdateValuesResponse> BatchUpdateAsync(SheetsService sheetsService, string spreadsheetId)
 		{
