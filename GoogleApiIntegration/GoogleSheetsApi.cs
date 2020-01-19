7c23accf01654a7fc84db1b9b63aa6d9b82e155d
Dmitri Mogilevski <dmogilevski@gmail.com>
2020-01-18 21:26:24 -0600

    Google Sheets API calls moved to GoogleSheetsApi class.

---
+++ GoogleApiIntegration/GoogleSheetsApi.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Newtonsoft.Json;
+
+namespace GoogleApiIntegration
+{
+	public class GoogleSheetsApi
+	{
+		private readonly SheetsService _sheetsService;
+
+		public GoogleSheetsApi(string appName, ServiceAccountCredential serviceAccountCredential)
+		{
+			_sheetsService = new SheetsService(new BaseClientService.Initializer
+			{
+				ApplicationName = "TempoApi",
+				HttpClientInitializer = serviceAccountCredential
+			});
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="title"></param>
+		/// <returns></returns>
+		public async Task<Spreadsheet> CreateAsync(string title)
+		{
+			var requestBody = new Spreadsheet
+			{
+				Properties = new SpreadsheetProperties
+				{
+					Title = "Jack's Great Spreadsheet"
+				}
+			};
+
+			SpreadsheetsResource.CreateRequest createRequest = _sheetsService.Spreadsheets.Create(requestBody);
+
+			Spreadsheet createResponse = await createRequest.ExecuteAsync();
+			Console.WriteLine("Create sheet response");
+
+			Console.WriteLine(JsonConvert.SerializeObject(createResponse));
+			return createResponse;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="spreadsheetId"></param>
+		/// <returns></returns>
+		public async Task<BatchUpdateValuesResponse> BatchUpdateAsync(string spreadsheetId)
+		{
+			// How the input data should be interpreted.
+			const string valueInputOption = "USER_ENTERED";
+
+			// The new values to apply to the spreadsheet.
+			var data = new List<ValueRange>
+			{
+				new ValueRange
+				{
+					Range = "Sheet1!A1:C1",
+					Values = new List<IList<object>>
+					{
+						new List<object>
+						{
+							"First name",
+							"Last name",
+							"Email"
+						}
+					}
+				},
+				new ValueRange
+				{
+					Range = "Sheet1!A2:C2",
+					Values = new List<IList<object>>
+					{
+						new List<object>
+						{
+							"Jack",
+							"Porter",
+							"someemail@something.com"
+						}
+					}
+				},
+				new ValueRange
+				{
+					Range = "Sheet1!A3:C3",
+					Values = new List<IList<object>>
+					{
+						new List<object>
+						{
+							"Tim",
+							"Eck",
+							"teck@something.com"
+						}
+					}
+				}
+			};
+
+			var requestBody = new BatchUpdateValuesRequest
+			{
+				ValueInputOption = valueInputOption,
+				IncludeValuesInResponse = true,
+				Data = data
+			};
+
+			SpreadsheetsResource.ValuesResource.BatchUpdateRequest batchUpdateRequest = _sheetsService.Spreadsheets.Values.BatchUpdate(requestBody, spreadsheetId);
+
+			BatchUpdateValuesResponse batchUpdateResponse = await batchUpdateRequest.ExecuteAsync();
+			Console.WriteLine("Batch update sheet response");
+			Console.WriteLine(JsonConvert.SerializeObject(batchUpdateResponse));
+
+			return batchUpdateResponse;
+		}
+	}
+}
--- GoogleSheets/Program.cs
+++ GoogleSheets/Program.cs
@@ -1,16 +1,13 @@
 ﻿using Google.Apis.Auth.OAuth2;
-using Google.Apis.Services;
-using Google.Apis.Sheets.v4;
-using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Drive.v3;
 using Google.Apis.Drive.v3.Data;
-using Google.Apis.Http;
-using Google.Apis.Util.Store;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4.Data;
+using GoogleApiIntegration;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using GoogleApiIntegration;
 
 namespace GoogleSheets
 {
@@ -19,16 +16,10 @@ namespace GoogleSheets
 		private static async Task Main(string[] args)
 		{
 			ServiceAccountCredential credential = GoogleCredentialManager.CreateGoogleCredential("tempo-api@tempo-265222.iam.gserviceaccount.com", "tempo-265222-88dada7b17c3.json");
-
-			var sheetsService = new SheetsService(new BaseClientService.Initializer
-			{
-				ApplicationName = "TempoApi",
-				HttpClientInitializer = credential
-			});
-
-			Spreadsheet createResponse = await CreateAsync(sheetsService);
-
-			BatchUpdateValuesResponse batchUpdateValuesResponse = await BatchUpdateAsync(sheetsService, createResponse.SpreadsheetId);
+			
+			var sheetsApi = new GoogleSheetsApi("TempoApi", credential);
+			Spreadsheet createResponse = await sheetsApi.CreateAsync("Jack's Great Spreadsheet");
+			BatchUpdateValuesResponse batchUpdateValuesResponse = await sheetsApi.BatchUpdateAsync(createResponse.SpreadsheetId);
 
 			var driveService = new DriveService(new BaseClientService.Initializer
 			{
@@ -39,7 +30,7 @@ namespace GoogleSheets
 			var fileList = await ListFilesAsync(driveService);
 
 			var permission = await ShareAsync(driveService, createResponse.SpreadsheetId, "dmitri.mogilevski@paradigmagency.com");
-			permission = await ShareAsync(driveService, createResponse.SpreadsheetId, "jackporter05@gmail.com");
+			// permission = await ShareAsync(driveService, createResponse.SpreadsheetId, "jackporter05@gmail.com");
 
 			Console.ReadKey();
 		}
@@ -98,101 +89,6 @@ namespace GoogleSheets
 			permission = await request.ExecuteAsync();
 
 			return permission;
-		}
-
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="sheetsService"></param>
-		/// <returns></returns>
-		private static async Task<Spreadsheet> CreateAsync(SheetsService sheetsService)
-		{
-			var requestBody = new Spreadsheet
-			{
-				Properties = new SpreadsheetProperties
-				{
-					Title = "Jack's Great Spreadsheet"
-				}
-			};
-
-			SpreadsheetsResource.CreateRequest createRequest = sheetsService.Spreadsheets.Create(requestBody);
-
-			Spreadsheet createResponse = await createRequest.ExecuteAsync();
-			Console.WriteLine("Create sheet response");
-
-			Console.WriteLine(JsonConvert.SerializeObject(createResponse));
-			return createResponse;
-		}
-
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="sheetsService"></param>
-		/// <param name="spreadsheetId"></param>
-		/// <returns></returns>
-		private static async Task<BatchUpdateValuesResponse> BatchUpdateAsync(SheetsService sheetsService, string spreadsheetId)
-		{
-			// How the input data should be interpreted.
-			const string valueInputOption = "USER_ENTERED";
-
-			// The new values to apply to the spreadsheet.
-			var data = new List<ValueRange>
-			{
-				new ValueRange
-				{
-					Range = "Sheet1!A1:C1",
-					Values = new List<IList<object>>
-					{
-						new List<object>
-						{
-							"First name",
-							"Last name",
-							"Email"
-						}
-					}
-				},
-				new ValueRange
-				{
-					Range = "Sheet1!A2:C2",
-					Values = new List<IList<object>>
-					{
-						new List<object>
-						{
-							"Jack",
-							"Porter",
-							"someemail@something.com"
-						}
-					}
-				},
-				new ValueRange
-				{
-					Range = "Sheet1!A3:C3",
-					Values = new List<IList<object>>
-					{
-						new List<object>
-						{
-							"Tim",
-							"Eck",
-							"teck@something.com"
-						}
-					}
-				}
-			};
-
-			var requestBody = new BatchUpdateValuesRequest
-			{
-				ValueInputOption = valueInputOption,
-				IncludeValuesInResponse = true,
-				Data = data
-			};
-
-			SpreadsheetsResource.ValuesResource.BatchUpdateRequest batchUpdateRequest = sheetsService.Spreadsheets.Values.BatchUpdate(requestBody, spreadsheetId);
-
-			BatchUpdateValuesResponse batchUpdateResponse = await batchUpdateRequest.ExecuteAsync();
-			Console.WriteLine("Batch update sheet response");
-			Console.WriteLine(JsonConvert.SerializeObject(batchUpdateResponse));
-
-			return batchUpdateResponse;
 		}
 	}
 }
